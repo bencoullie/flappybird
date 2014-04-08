db67f5be37577cf7660d82673fe219f057c86bc6
Ben Coullie <ben.coullie@43355.auckland.natcoll.ac.nz>
2014-04-08 14:21:51 +1200

    minor changes

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ var main_state = {
 
      //add gravity to the bird to make the bird fall
      this.bird.body.gravity.y = 1000;
+     this.bird.anchor.setTo(-0.2, 0.5); 
 
      // Call the 'jump' function when the spacekey is hit
     var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
@@ -44,11 +45,23 @@ var main_state = {
 
     this.game.physics.overlap(this.bird, this.pipes, this.restart_game, null, this);
 
+    if (this.bird.angle < 20)  
+    this.bird.angle++;
+
     },
 
     jump: function() {
      //add a vertical velocity to the bird
      this.bird.body.velocity.y = -350;
+
+     // create an animation on the bird
+    var animation = this.game.add.tween(this.bird);
+
+    // Set the animation to change the angle of the sprite to -20° in 100 milliseconds
+    animation.to({angle: -20}, 100);
+
+    // And start the animation
+    animation.start();  
     },
 
     restart_game: function() {
